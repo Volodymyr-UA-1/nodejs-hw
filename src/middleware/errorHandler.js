1e2cdcfa844646ef6d8a5a1ba8ac9bb6491de311
Volodymyr Salashchenko <salashchenkovladymyr@gmail.com>
2026-03-23 10:29:20 +0200

    add ENV and createHttpEroor

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,17 +1,17 @@
-import { HttpError } from "http-errors";
+import createHttpError from "http-errors";
 
 // Middleware для обробки помилок
 export const errorHandler = ((err, req, res, next) => {
     console.error("Error Middleware:", err);
 
     // Якщо помилка створена через http-errors
-    if (err instanceof HttpError) {
+    if (err instanceof createHttpError) {
         return res.status(err.status).json({
             message: err.message || err.name,
         });
     }
 
-    const isProd = process.env.MONGO_URL === "production";
+    const isProd = process.env.NODE_ENV === "production";
 
     res.status(500).json({
         message: isProd
