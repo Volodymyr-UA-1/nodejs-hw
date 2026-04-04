08315e0fc3534bdbe745684678cdf3a7b14f8359
Volodymyr Salashchenko <salashchenkovladymyr@gmail.com>
2026-04-04 23:24:11 +0300

    created 04-auth, add user.js, time,js, session.js, auth.js

---
+++ src/controllers/authController.js
@@ -0,0 +1,49 @@
+import bcrypt from 'bcrypt';
+import createHttpError from 'http-errors';
+import { User } from '../models/user.js';
+import { Session } from '../models/session.js';
+import { createSession, setSessionCookies } from '../services/auth.js';
+
+export const registerUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    throw createHttpError(400, 'Email in use');
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({
+    email,
+    password: hashedPassword,
+  });
+
+  const session = await createSession(newUser._id);
+
+  setSessionCookies(res, session);
+
+  res.status(201).json(newUser);
+};
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw createHttpError(401, 'Invalid credentials');
+  }
+
+  const isValidPassword = await bcrypt.compare(password, user.password);
+  if (!isValidPassword) {
+    throw createHttpError(401, 'Invalid credentials');
+  }
+
+  await Session.deleteOne({ userId: user._id });
+
+  const session = await createSession(user._id);
+
+  setSessionCookies(res, session);
+
+  res.status(200).json(user);
+};
---
+++ src/models/user.js
@@ -0,0 +1,27 @@
+import { model, Schema } from 'mongoose';
+
+const userSchema = new Schema(
+  {
+    username: { type: String, trim: true },
+    email: { type: String, unique: true, required: true, trim: true },
+    password: { type: String, required: true, minlength: 8 },
+  },
+  { timestamps: true },
+);
+
+// метод
+userSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+
+// хук
+userSchema.pre('save', function (next) {
+  if (!this.username) {
+    this.username = this.email;
+  }
+  next();
+});
+
+export const User = model('User', userSchema);
